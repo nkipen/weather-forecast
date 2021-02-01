787f62dc4895c2912786c70ec21f1a2552e0f76f
nkipen <nataliia.kipen@gmail.com>
2021-02-01 17:33:08 +0200

    WF-widget | add styles & icons

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,10 @@
 </template>
 
 <style lang="scss">
+@import '~@/assets/fonts/fonts.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/MiniWidget.vue
+++ src/components/MiniWidget.vue
@@ -4,7 +4,10 @@
     <div class="weather-item">
       <div class="weather-item__name">{{ cityName }}</div>
       <div class="weather-item__temp">{{ temperature }}<div>°C</div></div>
-      <div>{{ weather.description }}</div>
+      <div class="weather-item__description">
+        <img :src="imgSrc" class="weather-icon" alt="weather-icon">
+        {{ weather.description }}
+      </div>
     </div>
   </div>
 </template>
@@ -17,10 +20,13 @@ export default defineComponent({
   name: 'MiniWidget',
   setup () {
     const store = useStore()
+    const weather = computed(() => store.getters.getCurrentWeather.weather)
+    const icon = computed(() => store.getters.getCurrentWeather.weather.icon)
     return {
-      weather: computed(() => store.getters.getCurrentWeather.weather),
+      weather,
       cityName: computed(() => store.getters.getCurrentWeather.city_name),
-      temperature: computed(() => store.getters.getCurrentWeather.temp)
+      temperature: computed(() => store.getters.getCurrentWeather.temp),
+      imgSrc: computed(() => require(`@/assets/icons/${icon.value}.png`))
     }
   },
   props: {
@@ -36,7 +42,13 @@ export default defineComponent({
 .weather-item {
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
   margin: 0 auto;
+
+  & div {
+    display: flex;
+  }
 
   &__temp {
     display: flex;
@@ -44,10 +56,22 @@ export default defineComponent({
     line-height: .9;
     justify-content: center;
     margin: 1rem 0;
+    color: #42b983;
 
     div {
       font-size: 2rem;
+      margin-top: .8rem;
+      color: #2c3e50;
     }
+  }
+
+  &__description {
+    align-items: center;
+  }
+
+  .weather-icon {
+    width: 50px;
+    height: 50px;
   }
 }
 
